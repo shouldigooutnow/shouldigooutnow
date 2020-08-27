9aaf1cb9144fdd87fec9c1f8ac607e4649ec28c7
Irfan Nazir <i@irfy.co.uk>
2020-08-27 16:26:20 +0200

    Make model slightly smarter

--- src/model/index.js
+++ src/model/index.js
@@ -3,20 +3,25 @@ import { PROB_COVID, PROB_TRANSMISSION } from './assumptions'
 
 // The probability that x happens exacltly k times out of n
 const binomialProbability = (trials, successes, prob_of_success) => {
-  console.log('called bin prob')
-  return combinations(trials, successes) * Math.pow(prob_of_success, successes) * Math.pow(1 - prob_of_success, trials - successes)
+  const p = Math.min(1, prob_of_success)
+  return combinations(trials, successes) * Math.pow(p, successes) * Math.pow(1 - p, trials - successes)
 }
 
-// // The probability that x happens upto k times out of n
-// const cumulativeBinomialProbability = (trials, successes, prob_of_success) => {
-//   let cumulative = 0
+// The probability that x happens upto k times out of n
+const cumulativeBinomialProbability = (trials, successes, prob_of_success) => {
+  let cumulative = 0
 
-//   for (let i = 0; i <= successes; ++i) {
-//     cumulative = cumulative + binomialProbability(trials, i, prob_of_success)
-//   }
+  for (let i = 0; i <= successes; ++i) {
+    cumulative = cumulative + binomialProbability(trials, i, prob_of_success)
 
-//   return cumulative
-// }
+    if (cumulative >= 1) {
+      cumulative = 1
+      break
+    }
+  }
+
+  return cumulative
+}
 
 const calcExpectedNumberOfPeopleWithCovid = (numberOfPeople, covidProbability) => {
   return numberOfPeople * covidProbability
@@ -30,15 +35,14 @@ const calcExpectedNumberOfPeopleWithCovid = (numberOfPeople, covidProbability) =
 //    - if more people have covid, the chances you will get it increase
 const infectionProbability = (numberOfPeople, covidProbability, hourlyTransmissionProbability, eventDurationMins) => {
   const expectedNumberOfPeopleWithCovid = calcExpectedNumberOfPeopleWithCovid(numberOfPeople, covidProbability)
-  return expectedNumberOfPeopleWithCovid
+  const expectedInfectionRate = cumulativeBinomialProbability(numberOfPeople, numberOfPeople, covidProbability)
+  return expectedNumberOfPeopleWithCovid * Math.min(1, expectedInfectionRate)
 }
 
 const calculateCovidProb = activity => {
-  console.log(activity)
-  console.log(PROB_TRANSMISSION[activity.activity])
   const probSomeonePresentHasCovid = binomialProbability(activity.numberOfPeoplePresent, 1, PROB_COVID)
   const probContractingCovid = infectionProbability(
-    activity.numberOfPeople,
+    activity.numberOfPeoplePresent,
     PROB_COVID,
     PROB_TRANSMISSION[activity.activity],
     activity.durationMins
@@ -60,6 +64,6 @@ export const calculateCovidProbs = activities => {
 export const calculateHighLevelProbs = activitiesWithProbs => {
   return {
     probSomeonePresentHasCovid: 1 - activitiesWithProbs.reduce((result, activity) => result * activity.probNobodyPresentHasCovid, 1),
-    probContractingCovid: 1 - activitiesWithProbs.reduce((result, activity) => result * activity.probNotContractingCovid, 1)
+    probContractingCovid: Math.min(1, 1 - activitiesWithProbs.reduce((result, activity) => result * activity.probNotContractingCovid, 1))
   }
 }
