4f411bd8d8855e84d6337ec2cb95b2451d7420df
Richard Gill <richard@rgill.co.uk>
2020-08-27 16:01:12 +0100

    added routing

---
+++ src/components/router.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Landing from '@/components/pages/landing'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import URI from 'urijs'
+
+const routes = [
+  {
+    path: '/',
+    component: Landing
+  }
+]
+
+export const Router = () => (
+  <BrowserRouter>
+    <Switch>
+      {routes.map(route => {
+        return (
+          <Route
+            key={route.path}
+            path={route.path}
+            render={routerProps => {
+              console.log('x')
+              const queryParams = URI()
+                .query(routerProps.history.location.search)
+                .query(true)
+              console.log(route)
+              const Component = route.component
+              const props = { ...route.props, ...routerProps, queryParams }
+              return <Component {...props} />
+            }}
+          />
+        )
+      })}
+    </Switch>
+  </BrowserRouter>
+)
