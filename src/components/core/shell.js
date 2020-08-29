141d6e4ebd981f28900c5b0653612c5e4882aab8
Richard Gill <richard@rgill.co.uk>
2020-08-29 10:48:57 +0100

    tweaked footer colors again

--- src/components/core/shell.js
+++ src/components/core/shell.js
@@ -4,15 +4,15 @@ import { Github, Heart } from '@/components/icons'
 export const Shell = props => (
   <div className="min-h-full flex flex-col">
     <div className="container mx-auto py-6 flex-1">{props.children}</div>
-    <div className="w-full bg-teal-400">
+    <div className="w-full bg-teal-500">
       <div className="container flex flex-wrap items-center justify-between mx-auto py-4 flex-1">
-        <p className="mr-2">
+        <p className="mr-2 text-white">
           Made with <Heart className="inline mb-1 text-red-500" /> by Richard and Irfan{' '}
         </p>
         <a href="https://github.com/shouldigooutnow/shouldigooutnow/">
           <div className="flex flex-row items-center">
-            <p className="mr-2">See the code</p>
-            <Github className="inline" />
+            <p className="mr-2 text-white">See the code</p>
+            <Github className="inline text-white" />
           </div>
         </a>
       </div>
