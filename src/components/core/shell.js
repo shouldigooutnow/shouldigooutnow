a7f1261a41ff8b5e863621303b8f8ffca95c0df5
Richard Gill <richard@rgill.co.uk>
2020-08-29 07:58:33 +0100

    fixed footer on phones

--- src/components/core/shell.js
+++ src/components/core/shell.js
@@ -6,7 +6,7 @@ export const Shell = props => (
     <div className="container mx-auto py-6 flex-1">{props.children}</div>
     <div className="w-full bg-teal-200">
       <div className="container flex flex-wrap items-center justify-between mx-auto py-4 flex-1">
-        <p>
+        <p className="mr-2">
           Made with <Heart className="inline mb-1 text-red-500" /> by Richard and Irfan{' '}
         </p>
         <a href="https://github.com/shouldigooutnow/shouldigooutnow/">
