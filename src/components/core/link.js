fdaa11667c219e2d064d64bf01fdb3ec35f87e9c
Irfan Nazir <i@irfy.co.uk>
2020-08-30 12:45:58 +0200

    Updated footer with linkedin links

--- src/components/core/link.js
+++ src/components/core/link.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import classNames from 'classnames'
 
 /* eslint-disable-next-line jsx-a11y/anchor-has-content */
-export const Link = props => <a {...props} className={classNames('text-teal-600', props.className)} />
+export const Link = props => <a {...props} className={props.className || 'text-teal-600'} />
