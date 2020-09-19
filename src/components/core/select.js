7fd2f162e3e7720e21ad52d63af03456d33af22d
Richard Gill <richard@rgill.co.uk>
2020-09-19 17:09:00 +0100

    fixed select visual glitch

--- src/components/core/select.js
+++ src/components/core/select.js
@@ -5,7 +5,7 @@ export const Select = props => (
   <select
     {...props}
     className={classNames(
-      'block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 mb-4 px-3 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline',
+      'block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 mb-4 pl-2 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline',
       props.className
     )}
   />
